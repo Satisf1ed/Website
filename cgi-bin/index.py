edbb7a70dde6bb596168497024992d2de7e5dcb4
Andrey <zhogov.andrey.d@gmail.com>
2020-02-29 17:18:35 +0300

    first commit

---
+++ cgi-bin/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/index/<title>')
+@app.route('/<title>')
+def index(title):
+    return render_template('templates/base.html', title=title)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
